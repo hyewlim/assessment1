7274316ff0d61a6d5207743ad73c0ff075df0daa
hyewlim <hyew.lim@gmail.com>
2022-06-27 22:39:54 +0800

    hello

--- src/main/java/vttp2002/workshop/Main.java
+++ src/main/java/vttp2002/workshop/Main.java
@@ -1,33 +1,51 @@
 package vttp2002.workshop;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main 
 {
-    static int port;
-    static String path;
     
     public final static int DEFAULT_PORT = 3000;
     public static void main( String[] args )
 
     {
+        int port;
+        Path p1 = Paths.get("/target");
 
-        if (args[0].contains("port")) {
+        if (args.length == 0) {
+            port = DEFAULT_PORT;  
+        }
+
+        if (args.length == 2 && args[0].contains("port")) {
             port = Integer.parseInt(args[1]);
-        } else {
-            port = DEFAULT_PORT;
         }
 
-        if (args[0].contains("docRoot")) {
-            path = args[1];
-        } else {
-            path = "./target";
+        if (args.length == 2 && args[0].contains("docRoot")) {
+            Path inputPath = Paths.get(args[1]);
         }
+
+        if (args.length == 4 && args[0].contains("port") && args[2].contains("docRoot")) {
+            port = Integer.parseInt(args[1]);
+            Path inputPath = Paths.get(args[3]);
+        }
+
         
-        ExecutorService executor = Executors.newFixedThreadPool(3);
-        executor.execute(HttpServer.start(port));
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        ServerSocket server = new ServerSocket(port);
+
+        while(true){
+            System.out.println("Waiting for client connection");
+            Socket sock = server.accept();
+            System.out.println("Connected ...");
+            CookieClientHandler thr = new CookieClientHandler(sock, cookieFile);
+            threadPool.submit(thr);
+            System.out.println("Submitted to threadpool");
+        }
         
 
     }
